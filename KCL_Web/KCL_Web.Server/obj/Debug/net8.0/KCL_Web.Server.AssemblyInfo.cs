8749ad136c68f79f50488bd958512d57c7178dc5
NhatTimo <nguyenvannhatltqb@gmail.com>
2024-05-08 13:34:17 +0700

    commit

--- KCL_Web/KCL_Web.Server/obj/Debug/net8.0/KCL_Web.Server.AssemblyInfo.cs
+++ KCL_Web/KCL_Web.Server/obj/Debug/net8.0/KCL_Web.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KCL_Web.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca80aabcf7d34664f2c886d3692d1d2f4727aad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c49e5d0524098f85e3c9d1cd4df52dbb9121699")]
 [assembly: System.Reflection.AssemblyProductAttribute("KCL_Web.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KCL_Web.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
