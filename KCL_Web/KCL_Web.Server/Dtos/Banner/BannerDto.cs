564d0b3aa5fabd0ccf0bde5dae2d6b5c6e0ed0b7
NhatTimo <nguyenvannhatltqb@gmail.com>
2024-05-10 09:06:17 +0700

    Hoàn thành Api Banner

---
+++ KCL_Web/KCL_Web.Server/Dtos/Banner/BannerDto.cs
@@ -0,0 +1,11 @@
+namespace KCL_Web.Server.Dtos.Banner
+{
+    public class BannerDto
+    {
+        public int BannerId { get; set; }
+
+        public string? BannerName { get; set; }
+
+        public string? ImageData { get; set; }
+    }
+}
---
+++ KCL_Web/KCL_Web.Server/Dtos/Banner/CreateBannerRequestDto.cs
@@ -0,0 +1,9 @@
+namespace KCL_Web.Server.Dtos.Banner
+{
+    public class CreateBannerRequestDto
+    {
+        public string? BannerName { get; set; }
+
+        public string? ImageData { get; set; }
+    }
+}
