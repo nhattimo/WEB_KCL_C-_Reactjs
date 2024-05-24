47451ef9bd95f3255fbb2083716f989de3f27030
SanNHT <nguyen.h.thaisan@gmail.com>
2024-05-24 11:27:09 +0700

    cap nhat moi nhat

--- KCL_Web/kcl_web.client/src/components/About.jsx
+++ KCL_Web/kcl_web.client/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import AboutCard from "./mutiAbout/aboutCard";
 
 function About() {
   return (
@@ -89,137 +89,10 @@ function About() {
 
 
   {/* About Start */}
-  <div className="container-fluid py-5">
-    <div className="container py-5">
-      <div className="row g-5 align-items-center">
-        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-          <div className="about-img">
-            <img className="img-fluid" src="../../public/image/about-img.jpg" />
-          </div>
-        </div>
-        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-          {/* <div className="btn btn-sm border rounded-pill text-primary px-3 mb-3">
-            
-          </div> */}
-          <h1 className="mb-4">Tầm Nhìn </h1>
-          <p className="mb-4">
-          Trở thành Công ty đáng tin cậy tại Miền Trung Tây Nguyên trong việc cung cấp các sản phẩm,
-           dịch vụ, giải pháp ứng dụng Công nghệ Thông tin trong lĩnh vực Chăm sóc Sức khỏe cá nhân và Quản lý Y tế
-
-          </p>
-          {/* <p className="mb-4">
-            Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et
-            sit, sed stet no labore lorem sit. Sanctus clita duo justo et
-            tempor.
-          </p> */}
-          <div className="d-flex align-items-center mt-4">
-            <a className="btn btn-primary rounded-pill px-4 me-3" href="">
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="container py-5">
-      <div className="row g-5 align-items-center">
-        <div className="col-lg-6 wow fadeIn order-lg-last" data-wow-delay="0.1s">
-          <div className="about-img">
-            <img className="img-fluid" src="../../public/image/" />
-          </div>
-        </div>
-        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-          <div className="btn btn-sm border rounded-pill text-primary px-3 mb-3">
-            About Us
-          </div>
-          <h1 className="mb-4">Sứ Mệnh </h1>
-          <p className="mb-4">
-          Với triết lý phụng sự từ tâm, chúng tôi đam mê tạo ra những sản phẩm có giá trị trong việc nâng cao chất lượng Quản lý Y tế và Chăm sóc Sức khỏe cá nhân Chủ động - Toàn diện - Trọn đời cho người Việt, vì chất lượng cuộc sống người Việt 
-
-          </p>
-          <p className="mb-4">
-            Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et
-            sit, sed stet no labore lorem sit. Sanctus clita duo justo et
-            tempor.
-          </p>
-          <div className="d-flex align-items-center mt-4">
-            <a className="btn btn-primary rounded-pill px-4 me-3" href="">
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-
-
-
-    <div className="container py-5">
-      <div className="row g-5 align-items-center">
-        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-          <div className="about-img">
-            <img className="img-fluid" src="../../public/image/about-img.jpg" />
-          </div>
-        </div>
-        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-          <div className="btn btn-sm border rounded-pill text-primary px-3 mb-3">
-            About Us
-          </div>
-          <h1 className="mb-4">We Make </h1>
-          <p className="mb-4">
-            Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor
-            sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem
-            et sit, sed stet no labore lorem sit. Sanctus clita duo justo et
-            tempor eirmod magna dolore erat amet
-          </p>
-          <p className="mb-4">
-            Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et
-            sit, sed stet no labore lorem sit. Sanctus clita duo justo et
-            tempor.
-          </p>
-          <div className="d-flex align-items-center mt-4">
-            <a className="btn btn-primary rounded-pill px-4 me-3" href="">
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="container py-5">
-      <div className="row g-5 align-items-center">
-        <div className="col-lg-6 wow fadeIn order-lg-last" data-wow-delay="0.1s">
-          <div className="about-img">
-            <img className="img-fluid" src="../../public/image/about-img.jpg" />
-          </div>
-        </div>
-        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-          <div className="btn btn-sm border rounded-pill text-primary px-3 mb-3">
-            About Us
-          </div>
-          <h1 className="mb-4">We Make </h1>
-          <p className="mb-4">
-            Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor
-            sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem
-            et sit, sed stet no labore lorem sit. Sanctus clita duo justo et
-            tempor eirmod magna dolore erat amet
-          </p>
-          <p className="mb-4">
-            Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et
-            sit, sed stet no labore lorem sit. Sanctus clita duo justo et
-            tempor.
-          </p>
-          <div className="d-flex align-items-center mt-4">
-            <a className="btn btn-primary rounded-pill px-4 me-3" href="">
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-      </div>
-
-
+  <div className="container py-5">
+    <AboutCard title="About Us" subtitle="Lorem ipsum dolor sit amet elit. Phasellus imperdiet, nulla et dictum interdum." 
+    text="Tempor erat elit Scelerisque vitae pellentesque sit amet porttitor eget dolor. Justo nec ultrices dui sapien eget mi proin sed libero enim sed"
+    footer="Copyright &copy; Your Website 2021. All Rights Reserved."/>
   </div>
   {/* About End */}
 </>
